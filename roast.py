b5d197cb935cf51bf8e891d5dd4ead152e08aa22
Pranav Kumar Tiwari <pkt@BLRETV-PXVVK6YW.local>
2024-10-25 13:25:57 +0530

    optimize linkedin profile

--- roast.py
+++ roast.py
@@ -11,49 +11,29 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
-def get_gemini_response(input, image, prompt):
-    response = model.generate_content([input, image[0], prompt])
+def get_gemini_response(input, url, user_prompt):
+    response = model.generate_content([input, url, user_prompt])
     return response.text
-
-def input_image_details(uploaded_file):
-    if uploaded_file is not None:
-        bytes_data = uploaded_file.getvalue()
-
-        image_parts = [
-            {
-                "mime_type": uploaded_file.type,
-                "data": bytes_data
-            }
-        ]
-
-        return image_parts
-    else:
-        raise FileNotFoundError("No file uploaded")
     
-st.set_page_config(page_title="Food Scan")
+st.set_page_config(page_title="LinkedIn boost")
 
-st.header('Food Scan with Google Gemini')
-input = st.text_input("Input prompt: ", key='input')
-uploaded_file = st.file_uploader("Choose an image of the food or food table", type=["jpg", 'jpeg', 'png'])
-image = ""
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+st.header('Optimize your LinkedIn profile')
+user_input_prompt = st.text_input("Input prompt: ", key='input')
+if (user_input_prompt == ""):
+    user_input_prompt = "ignore this additional prompt"
+url = st.text_input("LinkedIn URL: ", key='url')
 
-submit = st.button("Scan the Food(s)")
+submit = st.button("Go!")
 
-input_prompt = """
-You have to identify different types of food in images. 
-The system should accurately detect and label various foods displayed in the image, providing the name 
-of the food and its location within the image (e.g., bottom left, right corner, etc.). Additionally, 
-the system should extract nutritional information and categorize the type of food (e.g., fruits, vegetables, grains, etc.) 
-based on the detected items. The output should include a comprehensive report or display showing the
-identified foods, their positions, names, and corresponding nutritional details.
+base_prompt = """
+You are a professional coach.
+You will be given a prompt and a linkedIn URL. Help with suggestions on how
+to optimize the linkedin profile for better appeal to all visitors. The
+optional prompt will give you additional cues on what the user is looking for.
 """
 
 if submit:
-    image_data = input_image_details(uploaded_file)
-    response = get_gemini_response(input_prompt, image_data, input)
-    st.subheader("Food Scan report: ")
+    response = get_gemini_response(base_prompt, url, user_input_prompt)
+    st.subheader("My suggestions: ")
     st.write(response)
 
